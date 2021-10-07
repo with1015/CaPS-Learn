83e626a9e99884b1bc8d9bc13607301c8ff43548
Hyunjoon Jeong <with1015@unist.ac.kr>
2021-10-07 16:28:18 +0900

    Fixing data loader

--- capslearn/torch/utils/imagenet_load.py
+++ capslearn/torch/utils/imagenet_load.py
@@ -10,7 +10,20 @@ def get_loader(batch_size=32, data_dir="./data", transforms=None):
     imagenet_train = torchvision.datasets.ImageFolder(root=data_dir + "/train", transform=transforms)
     imagenet_test = torchvision.datasets.ImageFolder(root=data_dir + "/val", transform=transforms)
 
-    train_loader = DataLoader(imagenet_train, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
-    test_loader = DataLoader(imagenet_test, batch_size=batch_size, shuffle=False, num_workers=8, pin_memory=True)
+    train_loader = DataLoader(imagenet_train,
+                            batch_size=batch_size,
+                            shuffle=True,
+                            num_workers=16,
+                            prefetch_factor=2,
+                            pin_memory=True)
+
+    test_loader = DataLoader(imagenet_test,
+                            batch_size=batch_size,
+                            shuffle=False,
+                            num_workers=16,
+                            prefetch_factor=2,
+                            pin_memory=True)
 
     return train_loader, test_loader
+
+
---
+++ capslearn/torch/utils/others.py
@@ -0,0 +1,87 @@
+import argparse
+import os
+import time
+
+
+def argument_parsing():
+    parser = argparse.ArgumentParser(description='PyTorch Training with CaPS-Learning System.')
+    parser.add_argument('data', metavar='DIR',
+                        help='path to dataset')
+    parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                        help='number of data loading workers (default: 4)')
+    parser.add_argument('--epochs', default=90, type=int, metavar='N',
+                        help='number of total epochs to run')
+    parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                        help='manual epoch number (useful on restarts)')
+    parser.add_argument('--num-minibatches', default=None, type=int,
+                        help="Number of minibatches to run")
+    parser.add_argument('--no-validate', dest='no_validate', action='store_true',
+                        help="No validation")
+    parser.add_argument('-b', '--batch-size', default=256, type=int,
+                        metavar='N',
+                        help='mini-batch size (default: 256), this is the total '
+                             'batch size of all GPUs on the current node when '
+                             'using Data Parallel or Distributed Data Parallel')
+    parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                        metavar='LR', help='initial learning rate', dest='lr')
+    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                        help='momentum')
+    parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
+                        metavar='W', help='weight decay (default: 1e-4)',
+                        dest='weight_decay')
+    parser.add_argument('-p', '--print-freq', default=10, type=int,
+                        metavar='N', help='print frequency (default: 10)')
+    parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                        help='path to latest checkpoint (default: none)')
+    parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                        help='evaluate model on validation set')
+    parser.add_argument('--world-size', default=-1, type=int,
+                        help='number of nodes for distributed training')
+    parser.add_argument('--seed', default=None, type=int,
+                        help='seed for initializing training. ')
+    parser.add_argument('--gpu', default=None, type=int,
+                        help='GPU id to use.')
+
+    return parser.parse_args()
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
