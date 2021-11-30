caf7a68521fdf44a77d893b6f0ee5b77b8a83849
Hyunjoon Jeong <with1015@unist.ac.kr>
2021-11-30 18:39:15 +0900

    DDP broadcast done

--- capslearn/run/distribute_test.py
+++ capslearn/run/distribute_test.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import torch.distributed as dist
 import torchvision
@@ -5,10 +6,19 @@ import torchvision.models as models
 
 from capslearn.torch.distributed_test2 import DistributedDataParallel
 
-
 cuda = torch.device("cuda:0")
 
-dist.init_process_group(backend='gloo', rank=0, world_size=2)
+os.environ['MASTER_ADDR'] = 'dumbo049'
+os.environ['MASTER_PORT'] = '38000'
 
-model = torch.tensor()
+rank = 0
+dist.init_process_group(backend='gloo', rank=rank, world_size=2)
+
+inputs = torch.rand(4).to(cuda)
+
+model = torch.nn.Linear(4, 1).to(cuda)
+print(rank, list(model.parameters()))
 model = DistributedDataParallel(model, device_ids=[0])
+
+output = model(inputs)
+print(rank, list(model.parameters()))
--- capslearn/torch/distributed_test2.py
+++ capslearn/torch/distributed_test2.py
@@ -1,22 +1,21 @@
 import os
 import torch
 import torch.distributed as dist
-import torch.nn.modules import Module
+#import torch.nn.modules as Module
 
-from torch.multiprocessing. import Process
 from torch.distributed.distributed_c10d import _get_default_group
 
-class DistributedDataParallel(Module):
+class DistributedDataParallel(torch.nn.Module):
 
     def __init__(self, module,
-                 device_ids=None
+                 device_ids=None,
                  broadcast_buffers=True):
 
         super(DistributedDataParallel, self).__init__()
         self.module = module
         self.device_ids = device_ids
         self.broadcast_buffers = broadcast_buffers
-        self.require_forward_param_sync = broadcast_buffer
+        self.require_forward_param_sync = broadcast_buffers
         self.require_backward_param_sync = True
 
         self.modules_buffers = [list(self.module.buffers())]
@@ -25,17 +24,22 @@ class DistributedDataParallel(Module):
         self._tensor_list = [tensor for _, tensor in named_parameters]
         self.process_group = _get_default_group()
 
+        assert(self.process_group.size() > 0)
+
 
     def forward(self, *inputs, **kwargs):
         if self.require_forward_param_sync:
-            self._sync_params():
+            self._sync_params()
         return self.module(*inputs, **kwargs)
 
 
     def _sync_params(self):
+        if self.process_group.size() == 1:
+            return
         with torch.no_grad():
             if self.broadcast_buffers:
                 for param in self._tensor_list:
+                    param.requires_grad = False
                     self._broadcast_parameters(param)
 
 
